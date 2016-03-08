d0c3939153d31acbbc563944857b34b073217589
Tim Kennedy <tim.kennedy@tickbox.com.au>
2016-03-08 19:12:07 +1100

    Indent todoItem

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -6,83 +6,83 @@ const ENTER_KEY = 13;
 
 @observer
 export default class TodoItem extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-		this.state = {editText: props.todo.title};
+  constructor(props, context) {
+	super(props, context);
+	this.state = {editText: props.todo.title};
+  }
+
+  render() {
+	const {viewModel, todo} = this.props;
+	return (
+	  <li className={[
+		todo.completed ? "completed": "",
+		todo === viewModel.todoBeingEdited ? "editing" : ""
+	  ].join(" ")}>
+	  <div className="view">
+	  <input
+	  className="toggle"
+	  type="checkbox"
+	  checked={todo.completed}
+	  onChange={this.handleToggle}
+	  />
+	  <label onDoubleClick={this.handleEdit}>
+	  {todo.title}
+	  </label>
+	  <button className="destroy" onClick={this.handleDestroy} />
+	  </div>
+	  <input
+	  ref="editField"
+	  className="edit"
+	  value={this.state.editText}
+	  onBlur={this.handleSubmit}
+	  onChange={this.handleChange}
+	  onKeyDown={this.handleKeyDown}
+	  />
+	  </li>
+	);
+  }
+
+  handleSubmit = (event) => {
+	const val = this.state.editText.trim();
+	if (val) {
+	  this.props.todo.setTitle(val);
+	  this.setState({editText: val});
+	} else {
+	  this.handleDestroy();
 	}
+	this.props.viewModel.todoBeingEdited = null;
+  };
 
-	render() {
-		const {viewModel, todo} = this.props;
-		return (
-			<li className={[
-				todo.completed ? "completed": "",
-				todo === viewModel.todoBeingEdited ? "editing" : ""
-			].join(" ")}>
-				<div className="view">
-					<input
-						className="toggle"
-						type="checkbox"
-						checked={todo.completed}
-						onChange={this.handleToggle}
-					/>
-					<label onDoubleClick={this.handleEdit}>
-						{todo.title}
-					</label>
-					<button className="destroy" onClick={this.handleDestroy} />
-				</div>
-				<input
-					ref="editField"
-					className="edit"
-					value={this.state.editText}
-					onBlur={this.handleSubmit}
-					onChange={this.handleChange}
-					onKeyDown={this.handleKeyDown}
-				/>
-			</li>
-		);
-	}
+  handleDestroy = () => {
+	this.props.todo.destroy();
+	this.props.viewModel.todoBeingEdited = null;
+  };
 
-	handleSubmit = (event) => {
-		const val = this.state.editText.trim();
-		if (val) {
-			this.props.todo.setTitle(val);
-			this.setState({editText: val});
-		} else {
-			this.handleDestroy();
-		}
-		this.props.viewModel.todoBeingEdited = null;
-	};
-
-	handleDestroy = () => {
-		this.props.todo.destroy();
-		this.props.viewModel.todoBeingEdited = null;
-	};
-
-	handleEdit = () => {
-		const todo = this.props.todo;
-		this.props.viewModel.todoBeingEdited = todo;
+  handleEdit = () => {
+	const todo = this.props.todo;
+	  this.props.viewModel.todoBeingEdited = todo;
 		this.setState({editText: todo.title});
-	};
+  };
 
-	handleKeyDown = (event) => {
-		if (event.which === ESCAPE_KEY) {
-			this.setState({editText: this.props.todo.title});
-			this.props.viewModel.todoBeingEdited = null;
-		} else if (event.which === ENTER_KEY) {
-			this.handleSubmit(event);
-		}
-	};
+  handleKeyDown = (event) => {
+	if (event.which === ESCAPE_KEY) {
+	  this.setState({editText: this.props.todo.title});
+	  this.props.viewModel.todoBeingEdited = null;
+	} else if (event.which === ENTER_KEY) {
+	  this.handleSubmit(event);
+	}
+  };
 
-	handleChange = (event) => {
-		this.setState({editText: event.target.value});
-	};
+  handleChange = (event) => {
+	this.setState({editText: event.target.value});
+  };
 
-	handleToggle = () => {
-		this.props.todo.toggle();
-	};
+  handleToggle = () => {
+	this.props.todo.toggle();
+  };
 }
 
 TodoItem.propTypes = {
-	todo: React.PropTypes.object.isRequired,
-	viewModel: React.PropTypes.object.isRequired
+  todo: React.PropTypes.object.isRequired,
+  viewModel: React.PropTypes.object.isRequired
 };
