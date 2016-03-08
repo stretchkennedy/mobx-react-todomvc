29c0b1e08b97c3a56962b0b199a1135efd594ad8
Tim Kennedy <tim.kennedy@tickbox.com.au>
2016-03-08 19:10:44 +1100

    Remove unused utilities

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,3 @@
 export function pluralize(count, word) {
 	return count === 1 ? word : word + 's';
 }
-
-function randomFailurePromise(func) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			const r = Math.random()
-			if (r > 0.5) {
-				resolve(func())
-			} else {
-				reject("Random failure!")
-			}
-		}, Math.random * 300 + 300)
-	})
-}
-
-export function storeDataToLocalStore(namespace, data) {
-	return randomFailurePromise(() => {
-		if (data) {
-			localStorage.setItem(namespace, JSON.stringify(data));
-		}
-	})
-}
-
-export function getDataFromLocalStore(namespace) {
-	return randomFailurePromise(() => {
-		var store = localStorage.getItem(namespace);
-		return (store && JSON.parse(store)) || [];
-	})
-}
