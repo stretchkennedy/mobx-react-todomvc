31f13a356304ec85b8ae82e707be9ac5eaf463db
Tim Kennedy <tim.kennedy@tickbox.com.au>
2016-03-08 13:59:49 +1100

    Add json-server to stub an api

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+var childProcess = require('child_process')
+
+childProcess.spawn(
+  __dirname + '/node_modules/json-server/bin/index.js',
+  ['--port', '3001', __dirname + '/db.json'],
+  { stdio: 'inherit' }
+)
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  proxy: {
+    '/api/*': {
+      target: 'http://localhost:3001',
+      secure: false,
+      rewrite: function(req) {
+        req.url = req.url.replace(/^\/api/, '/')
+      }
+    }
+  }
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
